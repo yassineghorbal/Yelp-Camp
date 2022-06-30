a77814c4acc6ef14feaff6ca9df1ee473e88140c
yassineghorbal <yassineghorbal2000@gmail.com>
2022-06-30 19:46:05 +0100

    added bootstrap styles to all views

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate')
 
 const app = express()
 
@@ -15,11 +16,16 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
         console.log(err)
     })
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+    res.send('home will be added later')
+})
 
 //show new campground form
 app.get('/campgrounds/new', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 1000)
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eveniet molestiae id ut, amet asperiores mollitia laudantium est repellat sequi.',
+            price: price
         })
         await camp.save()
     }
