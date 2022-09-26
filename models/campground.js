ec54bea4550a244e12c058738b2152f55595ecc4
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-26 21:52:40 +0100

    added delete camp middleware to delete all reviews in camp doc

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -11,6 +12,16 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.remove({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
