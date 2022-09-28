e27f1bdb01e50639c44ab40089b1af32f0549aaf
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-28 21:18:52 +0100

    added users routes and register page

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const localStrategy = require('passport-local')
 const User = require('./models/user')
 
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+const userRoutes = require('./routes/users')
 
 connectDB()
 
@@ -64,8 +65,9 @@ app.get('/fakeuser', async (req, res) => {
     res.send(newUser)
 })
 
-app.use("/campgrounds", campgrounds)
-app.use("/campgrounds/:id/reviews", reviews)
+app.use('/', userRoutes)
+app.use("/campgrounds", campgroundRoutes)
+app.use("/campgrounds/:id/reviews", reviewRoutes)
 
 app.get('/', (req, res) => {
     res.send('home page')
