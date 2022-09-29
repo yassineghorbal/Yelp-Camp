96a00bb5692a94df7661d42cecd232cf23c6f9ec
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-29 12:25:08 +0100

    added logout endpoint

--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,12 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err) }
+        req.flash('success', 'Goodbye!')
+        res.redirect('/campgrounds')
+    })
+})
+
 module.exports = router
