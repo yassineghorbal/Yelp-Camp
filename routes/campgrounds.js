e19e51c6b12388d8a6b9dd98649955c3ccc2f1d3
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-30 11:52:26 +0100

    added author to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,6 +24,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 //save new campground
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
+    campground.author = req.user._id
     await campground.save()
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
@@ -37,7 +38,8 @@ router.get('/', catchAsync(async (req, res) => {
 
 //show single campground
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
+    console.log(campground)
     if (!campground) {
         req.flash('error', 'Cannot find that campground!')
         return res.redirect('/campgrounds')
--- seeds/index.js
+++ seeds/index.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground')
+const connectDB = require('../config/db')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
-        console.log(err)
-    })
+connectDB()
 
 const sample = (array) => {
     return array[Math.floor(Math.random() * array.length)]
@@ -22,6 +18,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 1000)
         const camp = new Campground({
+            author: '63358911738de89ce77c45cc',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
