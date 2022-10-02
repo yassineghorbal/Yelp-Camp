17bc8c2fe9fd7fb222816aa42a3fe1ff93505e99
yassineghorbal <yassineghorbal2000@gmail.com>
2022-10-02 19:18:55 +0100

    added geoData storage in mongodb

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,14 +18,13 @@ module.exports.createCampground = async (req, res, next) => {
         query: req.body.campground.location,
         limit: 1
     }).send()
-    console.log(geoData.body.features[0].geometry.coordinates)
-    res.send('ouiii')
-    // const campground = new Campground(req.body.campground)
-    // campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    // campground.author = req.user._id
-    // await campground.save()
-    // req.flash('success', 'Successfully made a new campground!')
-    // res.redirect(`/campgrounds/${campground._id}`)
+    const campground = new Campground(req.body.campground)
+    campground.geometry = geoData.body.features[0].geometry
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.author = req.user._id
+    await campground.save()
+    req.flash('success', 'Successfully made a new campground!')
+    res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampground = async (req, res) => {
