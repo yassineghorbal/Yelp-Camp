25b5f26a0dfdd0a794d47144127732c5efefff90
yassineghorbal <yassineghorbal2000@gmail.com>
2022-10-22 11:46:49 +0100

    fixed the map cluster

--- models/campground.js
+++ models/campground.js
@@ -1,59 +1,62 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const Review = require('./review')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Review = require('./review');
 
-const ImageSchema = new Schema(
-    {
-        url: String,
-        filename: String
-    }
-)
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
 
 ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200')
-})
+  return this.url.replace('/upload', '/upload/w_200');
+});
 
-const opts = { toJSON: { virtuals: true } }
+const opts = { toJSON: { virtuals: true }, timestamps: true };
 
-const CampgroundSchema = new Schema({
+const CampgroundSchema = new Schema(
+  {
     title: String,
     images: [ImageSchema],
     geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
-    reviews: [{
+    reviews: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }]
-}, { timestamps: true }, opts)
+        ref: 'Review',
+      },
+    ],
+  },
+  opts
+);
 
 CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
-    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>`
-})
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>`;
+});
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,54 +1,55 @@
-const mongoose = require('mongoose')
-const cities = require('./cities')
-const { places, descriptors } = require('./seedHelpers')
-const Campground = require('../models/campground')
-const connectDB = require('../config/db')
-const dotenv = require('dotenv').config()
-const colors = require('colors')
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+const connectDB = require('../config/db');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
 
-connectDB()
+connectDB();
 
-const sample = (array) => {
-    return array[Math.floor(Math.random() * array.length)]
-}
+const sample = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 const seedDB = async () => {
-    await Campground.deleteMany({})
-    for (let i = 0; i < 200; i++) {
-        const random1000 = Math.floor(Math.random() * 1000)
-        const price = Math.floor(Math.random() * 1000)
-        const camp = new Campground({
-            author: '633c309d1f43a4fc2f7872e6',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1664665215/YelpCamp/lnlszjkmfowjc6mrt7tf.jpg',
-                    filename: 'YelpCamp/lnlszjkmfowjc6mrt7tf',
-                },
-                {
-                    url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1664665220/YelpCamp/rdconztsc88nuhfllwfk.jpg',
-                    filename: 'YelpCamp/rdconztsc88nuhfllwfk',
-                },
-                {
-                    url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1664665219/YelpCamp/yneawh52tms5kd7znvct.jpg',
-                    filename: 'YelpCamp/yneawh52tms5kd7znvct',
-                }
-            ],
-            price: price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude
-                ]
-            }
-        })
-        await camp.save()
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 200; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      author: '633c309d1f43a4fc2f7872e6',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1666435091/YelpCamp/Camping_tent_REI_Co-op_Base_Camp_6_m_rlyayt.jpg',
+          filename: 'YelpCamp/Camping_tent_REI_Co-op_Base_Camp_6_m_rlyayt',
+        },
+        {
+          url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1666435094/YelpCamp/N-NIGHT-Glen_Bernard_Camp-2007_h2zeln.jpg',
+          filename: 'YelpCamp/N-NIGHT-Glen_Bernard_Camp-2007_h2zeln',
+        },
+        {
+          url: 'https://res.cloudinary.com/dtwuh88nw/image/upload/v1666435089/YelpCamp/tent-camping-at-sunset_ppaeb0.jpg',
+          filename: 'YelpCamp/tent-camping-at-sunset_ppaeb0',
+        },
+      ],
+      price: price,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
